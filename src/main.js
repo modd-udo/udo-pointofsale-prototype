4f24a07313a2eb3c6a695f721feae616a61145d9
Chris Seufert <chris@seufert.id.au>
2017-03-01 15:50:08 +1100

    added vuex store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App'
+import Vuex from 'vuex'
+import PosStore from './store'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  modules: {
+    pos: PosStore,
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
---
+++ src/store.js
@@ -0,0 +1,73 @@
+
+
+function emptySale() {
+  return {
+    status: "new",
+    customer: {
+      id: 0,
+      name: "",
+      email: "",
+      phone: "",
+      memberOf: [],
+      vouchers: [],
+      sales: [],
+    },
+    items: [],
+    payments: [],
+    promos: [],
+  };
+}
+
+export default {
+  namespaced: true,
+  state: {
+    screen: "Terminal",
+    saleId: 0,
+    saleList: [
+      emptySale(),
+    ]
+  },
+  getters: {
+
+  },
+  actions: {
+    newSale() {
+      commit('saleCreatred');
+    },
+    cancelSale({commit}) {
+      commit('saleRemoved');
+    },
+    holdSale({commit}) {
+      commit('saleRemoved');
+    },
+    finaliseSale({commit}) {
+    },
+    enterCode(code) {
+
+    },
+    addItem() {
+
+    },
+    setCustomer(user) {
+
+    },
+    createCustomer() {
+
+    },
+
+  },
+  mutations: {
+    saleCreated({state}) {
+      state.saleList.push(emptySale());
+      state.saleId = state.saleLlist.length - 1;
+    },
+    saleRemoved({state}) {
+      state.saleList.splice(state.saleId,1);
+      if(state.saleList.length == 0)
+        state.saleList.push(emptySale());
+      else if(state.saleList.length >= state.saleId)
+        state.saleId = state.saleList.length - 1;
+    },
+  },
+
+};
